059cafe7a25e0e9992e1f9191df44b6350fbe212
Diego González <dgonmej10@gmail.com>
2020-01-12 11:40:47 +0000

    Changed images, refactor and minor fixies

--- js/mode-selector.js
+++ js/mode-selector.js
@@ -2,73 +2,56 @@
 
 const toggle = document.querySelector('.js-change-theme');
 const body = document.querySelector('body');
-const profile = document.getElementById('profile');
+const profile = document.querySelectorAll('.profile');
+const profileDesktop = document.getElementById('profile-desktop');
+const profileMobile = document.getElementById('profile-mobile');
 const mainSection = document.getElementById('section-main');
 const bottomSection = document.getElementById('section-bottom');
 const logosContainer = document.getElementById('logos-container');
-const section = document.querySelectorAll('.section-theme')
-const box = document.querySelectorAll('.box-theme')
+const section = document.querySelectorAll('.section-theme');
+const box = document.querySelectorAll('.box-theme');
 
-const title = document.querySelectorAll('.title');
+function changeClass(childrens, item, classRemoved, classAdded) {
+    if (childrens) {
+        item.forEach(i => {
+            i.classList.remove(classRemoved);
+            i.classList.add(classAdded);
+        });
+    } else {
+        item.classList.remove(classRemoved);
+        item.classList.add(classAdded);
+    }
+};
 
 toggle.addEventListener('click', () => {
     if (body.classList.contains('text-gray-900')) {
         toggle.innerHTML = "<i class='fas fa-sun'></i>";
 
-        body.classList.remove('text-gray-900');
-        body.classList.add('text-gray-100');
-
-        profile.classList.remove('bg-white');
-        profile.classList.add('bg-gray-900');
+        profileDesktop.src = 'images/main_3.png';
+        changeClass(true, profile, 'bg-teal-700', 'bg-teal-900');
+        changeClass(false, profileMobile, 'bg-image-1', 'bg-image-2');
 
         mainSection.style.backgroundImage = "url('images/wallpaper_secondary.jpg')";
         bottomSection.style.backgroundImage = "url('images/wallpaper_secondary.jpg')";
-
-        logosContainer.classList.remove('bg-white');
-        logosContainer.classList.add('bg-gray-400');
-
-        section.forEach(item => {
-            item.classList.remove('bg-white');
-            item.classList.add('bg-black');
-        })
-
-        box.forEach(item => {
-            item.classList.remove('bg-white');
-            item.classList.add('bg-gray-900');
-        })
-
-        title.forEach(item => {
-            item.classList.remove('text-teal-700');
-            item.classList.add('text-gray-100');
-        })
+        
+        changeClass(true, box, 'bg-white', 'bg-gray-900');
+        changeClass(true, section, 'bg-white', 'bg-black');
+        changeClass(false, body, 'text-gray-900', 'text-gray-100');
+        changeClass(false, logosContainer, 'bg-white', 'bg-gray-400');
     } else {
         toggle.innerHTML = "<i class='fas fa-moon'></i>";
 
-        body.classList.remove('text-gray-100');
-        body.classList.add('text-gray-900');
-
-        profile.classList.remove('bg-gray-900');
-        profile.classList.add('bg-white');
-
+        profileDesktop.src = 'images/main_1.png';
+        changeClass(true, profile, 'bg-teal-900', 'bg-teal-700');
+        changeClass(false, profileMobile, 'bg-image-2', 'bg-image-1');
+        
         mainSection.style.backgroundImage = "url('images/wallpaper_alternative.jpg')";
         bottomSection.style.backgroundImage = "url('images/wallpaper_primary.jpg')";
-
-        logosContainer.classList.remove('bg-gray-400');
-        logosContainer.classList.add('bg-white');
-
-        section.forEach(item => {
-            item.classList.remove('bg-black');
-            item.classList.add('bg-white');
-        })
-
-        box.forEach(item => {
-            item.classList.remove('bg-gray-900');
-            item.classList.add('bg-white');
-        })
-
-        title.forEach(item => {
-            item.classList.remove('text-gray-100');
-            item.classList.add('text-teal-700');
-        })
+        
+        changeClass(true, box, 'bg-gray-900', 'bg-white');
+        changeClass(true, section, 'bg-black', 'bg-white');
+        changeClass(false, body, 'text-gray-100', 'text-gray-900');
+        changeClass(false, logosContainer, 'bg-gray-400', 'bg-white');
     }
 });
+
